7882f5bd8cfd8ec85a5458cf1acb3340ea9a7bf6
mromeroval <mauricio.romero@outlook.com>
2020-02-09 19:27:25 +0100

    Add get player by ID method

--- controllers/playerControllers.js
+++ controllers/playerControllers.js
@@ -15,6 +15,17 @@ export const addNewPlayer = (req, res) => {
   });
 };
 
+// Function for gettin player by ID
+export const getPlayerById = (req, res) => {
+  let PlayerId = req.params.PlayerId;
+  Player.findById(PlayerId, (error, Player) => {
+    if(error){
+      res.send(error)
+    }
+      res.json(Player)
+  })
+} 
+
 // Function for getting all the players
 export const getPlayers = (req, res) => {
   Player.find({}, (error, Player) => {
--- routes/soccerRoutes.js
+++ routes/soccerRoutes.js
@@ -1,9 +1,19 @@
-import { addNewPlayer, getPlayers } from '../controllers/playerControllers';
+import {
+  addNewPlayer,
+  getPlayers,
+  getPlayerById
+} from '../controllers/playerControllers';
 
 const routes = (app) => {
   app.route('/players')
+    // POST endpoint
     .post(addNewPlayer)
+    // GET endpoint
     .get(getPlayers)
+
+  // GET player by ID
+  app.route('/player/:PlayerId')
+  .get(getPlayerById)
 }
 
 export default routes;
