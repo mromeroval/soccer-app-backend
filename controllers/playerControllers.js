0105347d36ac767810650d38cc7553ff4260352e
mromeroval <mauricio.romero@outlook.com>
2020-02-09 19:01:35 +0100

    Add get players method

--- controllers/playerControllers.js
+++ controllers/playerControllers.js
@@ -3,6 +3,7 @@ import { PlayerSchema } from '../models/playerModels';
 
 const Player = mongoose.model('Player', PlayerSchema);
 
+// Function for adding a new player
 export const addNewPlayer = (req, res) => {
   let newPlayer = new Player(req.body);
   newPlayer.save((error, Player) => {
@@ -13,3 +14,13 @@ export const addNewPlayer = (req, res) => {
     }
   });
 };
+
+// Function for getting all the players
+export const getPlayers = (req, res) => {
+  Player.find({}, (error, Player) => {
+    if(error) {
+      res.send(error)
+    }
+    res.json(Player)
+  });
+}
--- routes/soccerRoutes.js
+++ routes/soccerRoutes.js
@@ -1,8 +1,9 @@
-import { addNewPlayer } from '../controllers/playerControllers';
+import { addNewPlayer, getPlayers } from '../controllers/playerControllers';
 
 const routes = (app) => {
   app.route('/players')
     .post(addNewPlayer)
+    .get(getPlayers)
 }
 
 export default routes;
