aa6951180e3c9726d874cc6ff2afa82b921a0140
mromeroval <mauricio.romero@outlook.com>
2020-02-09 13:22:23 +0100

    Add mongo connection  and bodyparser configurations

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import bodyparser from 'body-parser';
 
 const app = express();
 const PORT = 3000;
+
+// Mongo connection
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://locahost/soccerDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+// bodyparser
+app.use(bodyparser.urlencoded({ extended:true}));
+app.use(bodyparser.json());
 
 app.get('/',(req, res) => {
   res.send(`Soccer App is running on port ${PORT}`)
